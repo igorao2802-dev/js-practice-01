1cabf6bb97bee01f47606ed1b10057d136ebd029
igorao2802-dev <igorao2802@gmail.com>
2026-02-16 11:59:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+
+const themeButton = document.getElementById('theme-toggle');
+
+// Восстановление темы при перезагрузке страницы
+const savedTheme = localStorage.getItem('theme'); 
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+    if (themeButton) themeButton.textContent = 'Включить светлую тему';
+} else {
+    document.body.classList.remove('dark-theme');
+    if (themeButton) themeButton.textContent = 'Включить тёмную тему';
+}
+
+
 // ЗАДАНИЕ 1: Переключение темы
 
 // 1. Найдите кнопку по id и сохраните в переменную const
-const themeButton = document.getElementById('theme-toggle');
-
+console.log (themeButton);
 // 2. Добавьте слушатель события 'click'
 themeButton.addEventListener('click', function() {
     // 3. Переключите класс 'dark-theme' у body
@@ -11,9 +24,21 @@ themeButton.addEventListener('click', function() {
     // ЗАДАНИЕ 3: Работа с переменными (студент дописывает)
     // Подсказка: объявите переменную isDarkMode и проверьте,
     // есть ли у body класс 'dark-theme'
+   let isDarkMode = document.body.classList.contains('dark-theme');
+   localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+      
+   if (isDarkMode) {
+            themeButton.textContent = 'Включить светлую тему';
+   } 
+      else {
+            themeButton.textContent = 'Включить тёмную тему';
+      }
 
-    // Выведите в консоль тип данных isDarkMode
+   // Выведите в консоль тип данных isDarkMode
     // console.log(typeof isDarkMode);
+   console.log('isDarkMode = ', isDarkMode);
+   console.log('typeof isDarkMode = ', typeof isDarkMode);
+
 });
 
 
@@ -26,7 +51,11 @@ const appDiv = document.getElementById('app');
 appDiv.textContent = 'Текущая дата и время: ' + new Date().toLocaleString('ru-RU');
 
 // Вопрос для размышления: Почему мы используем textContent, а не innerHTML?
-
+/* Для текущей задачи не требуется изменять структуру документа. Время загрузки 
+страницы является статическим (не требуется отображать динамическое время). Поэтому 
+с точки зрения безопасности и скорости загрузки страницы textContent предпочтительнее 
+(подробнее в README.md).
+*/
 /* 
    🌟 УРОВЕНЬ PRO (Extra Challenge):
    1. Добавьте на кнопку иконку (например, 🌙 или ☀️).
